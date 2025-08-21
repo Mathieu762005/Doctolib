29c1c5c673ab5f5e84c361a87095218c296c0ab9
Mathieu <mathieu762005@gmail.com>
2025-08-21 11:54:00 +0200

    maj

---
+++ src/login.php
@@ -0,0 +1,75 @@
+<?php
+// var_dump($_POST);
+
+// Création de regeX
+$regName = "/^[a-zA-Zàèé\-]+$/";
+
+// Je ne lance qu'uniquement lorsqu'il y a un formulaire validée via la méthod POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // je créé un tableau d'erreurs vide car pas d'erreur
+    $errors = [];
+
+    if (isset($_POST["mdp"])) {
+        // on va vérifier si c'est vide
+        if (empty($_POST["mdp"])) {
+            // si c'est vide, je créé une erreur dans mon tableau
+            $errors['mdp'] = 'mot de passe obligatoire';
+        } else if (!preg_match($regName, $_POST["mdp"])) {
+            // si ça ne respecte pas la regeX
+            $errors['mdp'] = 'Caractère(s) non autorisé(s)';
+        }
+    }
+
+    if (isset($_POST["email"])) {
+        // on va vérifier si c'est vide
+        if (empty($_POST["email"])) {
+            // si c'est vide, je créé une erreur dans mon tableau
+            $errors['email'] = 'Mail obligatoire';
+        } else if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            // si mail non valide, on créé une erreur
+            $errors['email'] = 'Mail non valide';
+        }
+    }
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <main>
+        <div>
+            <h1 class="text-center mt-5 fw-bold">Formulaire</h1>
+        </div>
+        <div class="formulaire container border rounded-4 mt-5">
+            <form class="row g-3 p-5" method="POST" action="" novalidate>
+                <div class="col-12">
+                    <label for="inputAddress" class="form-label">E-mail</label><span class="ms-2 text-danger fst-italic fw-light"><?= $errors["email"] ?? '' ?></span>
+                    <input type="email" name="email" value="<?= $_POST["email"] ?? "" ?>" class="form-control" id="inputAddress">
+                </div>
+                <div class="col-md-6">
+                    <label for="inputPassword4" class="form-label">Mot de passe</label><span class="ms-2 text-danger fst-italic fw-light"><?= $errors["mdp"] ?? '' ?></span>
+                    <input type="password" name="mdp" value="<?= $_POST["mdp"] ?? "" ?>" class="form-control" id="inputPassword4">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Confirmer mon inscription</button>
+                </div>
+            </form>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+
+</html>
